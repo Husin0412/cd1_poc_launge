ab8bfff16f52feae4d941d2db626c09e3357c0f5
Husin <Muhamad.husin0412@gmail.com>
2020-10-03 13:10:49 +0700

    hallo course

---
+++ duty_other/bagas/index.php
@@ -0,0 +1,192 @@
+<!doctype html>
+<html lang="en">
+    <head>
+    <meta  name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' />
+    <link rel="stylesheet" href="./assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./assets/css/app.css">
+    <link rel="stylesheet" href="./assets/css/all.css">
+    <link rel="stylesheet" href="./assets/css/animate.css">
+    <link rel="stylesheet" href="./assets/css/owl.carousel.css">
+    <link href="https://fonts.googleapis.com/css2?family=Titillium+Web:wght@600&display=swap" rel="stylesheet">
+    </head>
+    <body class="main">
+        <div class="header" style="background-color: rgb(5,5,5);">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-12">
+                        <nav class="navbar navbar-expand-lg navbar-light">
+                            <a class="navbar-brand" href="#">
+                                <img src="./assets/img/PL99.png" height="40">
+                            </a>
+                            <button class="navbar-toggler tgbtn" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                              <span class="navbar-toggler-icon"></span>
+                            </button>
+                            <div class="collapse navbar-collapse" id="navbarText">
+                              <ul class="navbar-nav ml-auto">
+                                <li class="nav-item active">
+                                  <a class="nav-link text-light" href="#"><img class="faimg" src="./assets/img/gift.png"> Klaim Bonus</a>
+                                </li>
+                                <li class="nav-item">
+                                  <a class="nav-link text-light" href="#"><img class="faimg" src="./assets/img/info.png">  Bantuan</a>
+                                </li>
+                              </ul>
+                            </div>
+                          </nav>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="content">
+          <div class="container">
+              <div class="row">
+                <!-- <div class="col-sm-12"> -->
+                <div class="col-sm-6">
+                  <img src="./assets/img/cardss.png" alt="" class="cardsimg">
+                </div>
+                <div class="col-sm-6">
+                  <div class="promotxt">
+                  <h5>Dapatkan Promo Cashback Deposit 10%</h5>
+                  <h3>Daftar Sekarang!</h3>
+                </div>
+                <div class="row">
+                  <div class="col-md-4">
+                    <a href="#" class="btn buttons btn-custom">Daftar</a>
+                  </div>
+                  <div class="col-md-4">
+                    <a href="#" class="btn buttons btn-custom">Login</a>
+                  </div>
+                </div>
+                </div>
+              <!-- </div> -->
+              </div>
+              <div class="row">
+                <div class="col-sm-6">
+                  <img src="./assets/img/starl.png" class="star-l" alt="">
+                </div>
+                <div class="col-sm-6">
+                    <img src="./assets/img/starr.png" class="star-r" alt="">
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-md-5">
+                  <h2 class="jackpot">FREE JACKPOT</h2>
+                  <h4 class="bett">50 X NILAI BET</h4>
+                </div>
+                <div class="col-md-2">
+                  <img src="./assets/img/middle-line.png" class="mid-line" alt="">
+                </div>
+                <div class="col-md-5">
+                  <h2 class="megajp">MEGA JACKPOT</h2>
+                  <h4 class="bet">5,500,000,000</h4>
+                </div>
+              </div>
+            </div>
+            <hr class="new2">
+            <div class="container">
+            <div class="row">
+              <div class="form-group">
+              <div class="col-sm-12 flaxui imgg">
+                <div class="">
+                <div class="game">
+                  <img src="./assets/img/bbola.png" class="gameimg" alt="">
+                </div>
+                <div class="game2">
+                  <span class="txtgame">SPORTSBOOK</span>
+                </div>
+              </div>
+              <div class="">
+                <div class="game">
+                  <img src="./assets/img/casinot.png" class="gameimg" alt="">
+                </div>
+                <div class="game2">
+                  <span class="txtgame">LIVE CASINO</span>
+                </div>
+              </div>
+              <div class="">
+                <div class="game">
+                  <img src="./assets/img/as.png" class="gameimg" alt="">
+                </div>
+                <div class="game2">
+                  <span class="txtgame">POKER</span>
+                </div>
+              </div>
+              <div class="">
+
+                <div class="game">
+                  <img src="./assets/img/slots.png" class="gameimg" alt="">
+                </div>
+                <div class="game2">
+                  <span class="txtgame">SLOTS</span>
+                </div>
+              </div>
+              <div class="">
+
+                <div class="game">
+                  <img src="./assets/img/daddu.png" class="gameimg" alt="">
+                </div>
+                <div class="game2">
+                  <span class="txtgame">IDNLIVE</span>
+                </div>
+              </div>
+              <div class="">
+
+                <div class="game">
+                  <img src="./assets/img/kudda.png" class="gameimg" alt="">
+                </div>
+                <div class="game2">
+                  <span class="txtgame">VIRTUAL SPORT</span>
+                </div>
+              </div>
+              <div class="">
+
+                <div class="game">
+                  <img src="./assets/img/promoo.png" class="gameimg" alt="">
+                </div>
+                <div class="game2">
+                <span class="txtgame">PROMOSI</span>
+              </div>
+              </div>
+            </div>
+            </div>
+          </div>
+            </div>
+            <hr class="new2">
+            <div class="container">
+              <div class="row">
+                <div class="col-md-12">
+                  <h2 style="text-align:center; color: rgb(255,0,0);">POKERLOUNGE99</h2>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-sm-12">
+                  <p style="color: rgb(255,255,255); text-align:center;">PokerLounge99 sebagai agen poker online indonesia terbesar dan terpercaya yang sudah menggunakan sistem keamanan terkini yang selalu di perbaharui dan
+                  dilengkapi dengan perangkat keras dan lunak yang canggih kenyamanan, keamanan dan rahasia data pribadi Anda aman bersama PokerLounge99</p>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-sm-12">
+                  <img src="./assets/img/bank.png" alt="">
+                </div>
+              </div>
+            </div>
+        </div>
+        <div class="footer">
+          <hr class="new1">
+          <div class="container con-ft">
+            <div class="row">
+              <div class="ft">
+              <div class="col-sm-12">
+                  <p>&copy; 2019 POKELUNGE99 ALL RIGHTS RESERVED</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </body>
+    <script src="./assets/js/jquery.slim.min.js"></script>
+    <script src="./assets/js/jquery.min.js"></script>
+    <script src="./assets/js/marquee.min.js"></script>
+    <script src="./assets/bootstrap/js/popper.min.js"></script>
+    <script src="./assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="./assets/js/owl.carousel.min.js"></script>
+</html>
